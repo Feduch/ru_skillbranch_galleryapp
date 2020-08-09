d11a4563441245b65c2f87791e863ed083427672
Fedor Buzinov <fedor@lifext.app>
2020-08-09 14:07:39 +0300

    Тест

---
+++ lib/user_holder.dart
@@ -0,0 +1,51 @@
+import 'package:FlutterGalleryApp/models/user.dart';
+
+import 'string_utils.dart';
+
+class UserHolder {
+  Map<String, User> users = {};
+
+  void registerUser(String name, String phone, String email) {
+    User user = User(name: name, email: email, phone: phone);
+
+    print(user.toString());
+
+    if (!users.containsKey(user.login)) {
+      users[user.login] = user;
+    } else {
+      throw Exception("A user with this login already exists");
+    }
+  }
+
+  void registerUserByEmail(String name, String email) {
+    User user = User(name: name, email: email);
+
+    print(user.toString());
+
+    if (!users.containsKey(user.login)) {
+      users[user.login] = user;
+    } else {
+      throw Exception("A user with this email already exists");
+    }
+  }
+
+  void registerUserByPhone(String name, String phone) {
+    User user = User(name: name, phone: phone);
+
+    print(user.toString());
+
+    if (!users.containsKey(user.login)) {
+      users[user.login] = user;
+    } else {
+      throw Exception("A user with this phone already exists");
+    }
+  }
+
+  void findUserInFriends(String login) {
+
+  }
+
+  void importUsers(String fileName  ) {
+
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,25 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:galleryapp/main.dart';
+import '../lib/user_holder.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  UserHolder holder;
+  
+  setUp(() {
+    holder = UserHolder();
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  tearDownAll(() {
+    holder = null;
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('registerUser', () {
+    holder.registerUser("Test Iva", "+79000000096", "test@gmail.com");
+    
+    expect("test@gmail.com", holder.users["test@gmail.com"].login);
+    expect("+79000000096", holder.users["fedor.buzinov@gmail.com"].phone);
+    expect("fedor.buzinov@gmail.com", holder.users["test@gmail.com"].email);
+    expect("Test Iva", holder.users["test@gmail.com"].name);
   });
 }
